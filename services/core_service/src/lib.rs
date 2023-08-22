8399e31aefcc741a87833ebde68b05b0af1fd1b3
authName <authEmail>
2023-08-22 15:52:29 +0800

    change dir name to rs and add sa to compile
    Change-Id: Ia258fdbafea25f9d2150b43c81fa22f28e227b00
    
    Match-id-406907f08a3195f23ea970b857eb2fe47030cc6a

--- services/core_service/src/lib.rs
+++ services/core_service/src/lib.rs
@@ -15,31 +15,32 @@
 
 //! This create implement the asset
 
-use asset_common_lib::asset_type::{AssetMap, AssetTag};
-use asset_ipc_define_lib::asset_service::{AssetStub, AssetBroker};
+use asset_common_lib::asset_type::{AssetMap, AssetTag, AssetResult, AssetValue};
+use asset_ipc_define_lib::asset_service::{AssetStub, AssetBroker, ASSET_SERVICE_ID};
 
-use ipc_rust::add_service;
+use ipc_rust::{add_service, join_work_thread, IRemoteBroker};
 
-pub use access_token::init_access_token;
+use access_token::init_access_token;
 
 mod access_token;
 
+/// xxx
 pub struct AssetService;
+impl IRemoteBroker for AssetService {}
 
 impl AssetBroker for AssetService {
-    fn insert(&self, input: &AssetMap) -> asset_common_lib::asset_type::AssetResult<AssetMap> {
-        let map = AssetMap::new();
-        map.insert(AssetTag::AssetTagAuthType, 2); // to do
+    fn insert(&self, _input: &AssetMap) -> AssetResult<AssetMap> {
+        let mut map = AssetMap::new();
+        map.insert(AssetTag::AssetTagAuthType, AssetValue::NUMBER(2)); // to do
         Ok(map)
     }
 }
 
-fn asset_server_init() {
+/// xxx
+pub fn asset_server_init() {
     init_access_token();
     // create stub
     let service = AssetStub::new_remote_stub(AssetService).expect("create AssetService success");
-    add_service(&service.as_object().expect("get Asset service failed"),
-    EXAMPLE_ASSET_IPC_SERVICE_ID).expect("add server to samgr failed"); 
-    println!("join to ipc work thread");
+    add_service(&service.as_object().expect("get Asset service failed"), ASSET_SERVICE_ID).expect("add server to samgr failed"); 
     join_work_thread();   
 }
