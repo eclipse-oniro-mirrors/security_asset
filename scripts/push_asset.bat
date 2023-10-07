b4728bdd62a5825158535dbf63232effc08dfd12
authName <authEmail>
2023-10-07 10:36:14 +0800

    modify the ndk module and successfully test
    Change-Id: I217284725636b3a03ec7c8c3d4f9cdcc5c938dd2
    
    Match-id-7500c2593d366e79a7c04b32fcbbd3943f49ff63

--- scripts/push_asset.bat
+++ scripts/push_asset.bat
@@ -1,7 +1,7 @@
 hdc shell mount -o rw,remount /
 hdc file send \\7.222.59.152\z00639827\workspace\hmos_asset\out\generic_generic_arm_64only\hisi_higeneric_newphone_standard\lib.unstripped\security\asset\libasset_sdk.dylib.so ./system/lib64/
 hdc file send \\7.222.59.152\z00639827\workspace\hmos_asset\out\generic_generic_arm_64only\hisi_higeneric_newphone_standard\lib.unstripped\security\asset\libasset_sdk_ffi.z.so ./system/lib64/
-hdc file send \\7.222.59.152\z00639827\workspace\hmos_asset\out\generic_generic_arm_64only\hisi_higeneric_newphone_standard\lib.unstripped\security\asset\libasset_ndk.z.so ./system/lib64/
+hdc file send \\7.222.59.152\z00639827\workspace\hmos_asset\out\generic_generic_arm_64only\hisi_higeneric_newphone_standard\lib.unstripped\security\asset\libasset_ndk.z.so ./system/lib64/ndk/
 hdc file send \\7.222.59.152\z00639827\workspace\hmos_asset\out\generic_generic_arm_64only\hisi_higeneric_newphone_standard\lib.unstripped\security\asset\libasset_service.dylib.so ./system/lib64/
 hdc file send \\7.222.59.152\z00639827\workspace\hmos_asset\out\generic_generic_arm_64only\hisi_higeneric_newphone_standard\lib.unstripped\security\asset\libasset_napi.z.so ./system/lib64/module/security
 
