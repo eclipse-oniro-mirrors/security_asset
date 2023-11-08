ce42fafa262b23a807f4cd339ae0bd32ea7739c4
authName <authEmail>
2023-11-08 20:36:35 +0800

    add cov script
    Match-id-2151c4160cd8b667cb77ebc7473bc5becca3af35

---
+++ scripts/cov.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright Huawei Technologies Co., Ltd. 2010-2023. All rights reserved.
+
+set -e
+
+../../../../../OpenHarmony_prebuilts/linux/rust-1.68.0-dev-x86_64-unknown-linux-gnu/rust-1.68.0-dev-x86_64-unknown-linux-gnu/llvm-tools-preview/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata merge --sparse ../services/db_operator/*.profraw -o ../services/db_operator/default.profdata
+
+grcov ../services/db_operator/ -s ../services/db_operator --binary-path ../../../../out/generic_generic_arm_64only/hisi_higeneric_newphone_standard/exe.unstripped/tests/unittest/asset/asset_UT_test/asset_db_operator_test -t html --branch --excl-br-line "^\s*((debug_)?assert(_eq|_ne)?!|#\[derive\()" --ignore-not-existing -o ./report
