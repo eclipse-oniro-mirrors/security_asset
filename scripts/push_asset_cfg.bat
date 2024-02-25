495f4713db3ee2f954fcabb27de1e3090b4efac1
wind <youziting@huawei.com>
2024-02-25 18:09:00 +0800

    update copyright
    Signed-off-by: wind <youziting@huawei.com>

--- scripts/push_asset_cfg.bat
+++
@@ -1,6 +0,0 @@
-hdc shell mount -o rw,remount /
-hdc file send huks_service.json /system/profile/huks_service.json
-hdc shell mkdir /data/service/el1/public/asset_service
-hdc shell chown  huks_server /data/service/el1/public/asset_service
-hdc shell reboot
-pause
--- scripts/永久禁用selinux.bat
+++
@@ -1,9 +0,0 @@
-hdc shell mount -o rw,remount /
-hdc shell "sed -i 's/enforcing/permissive/g' /system/etc/selinux/config"
-hdc shell "cat /system/etc/selinux/config |grep SELINUX="
-
-@echo "Reboot device..."
-
-hdc shell reboot
-
-pause
